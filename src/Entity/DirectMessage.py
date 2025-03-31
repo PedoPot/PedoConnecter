25001a60b5753956a5079a5717ca83fdf3c450a0
LennyGonzales <lenny.gonzales.page@gmail.com>
2025-03-31 14:30:49 +0200

    feat(3): add Authentication entity

--- src/Entity/DirectMessage.py
+++ src/Entity/DirectMessage.py
@@ -1,9 +1,4 @@
-from pydantic import BaseModel
-from typing import Union
+from Entity.Authentication import Authentication
 
-class DirectMessage(BaseModel):
-    apiKey: Union[str, None] = None
-    apiSecret: Union[str, None] = None
-    accessToken: str
-    accessSecret: Union[str, None] = None
-    userId: Union[str, None] = None
+class DirectMessage(Authentication):
+    message: str
